4e421a18801e82ee1e0841443fc61f79c0c560ee
alealle <106605527+alealle@users.noreply.github.com>
2024-02-02 18:13:29 -0300

    break line in HelloArgs

--- HelloArguments.c
+++ HelloArguments.c
@@ -1,7 +1,10 @@
 #include "stdio.h"
 
 int main(int argc, char *argv[]) {
-    printf("%d\n",argc);
-    printf("Hello World!\n");
+    if (argc == 1) {
+        printf("Hello World!\n");
+        return 0;
+    }
+    printf("Hello, %s\n", argv[1]);
     return 0;
 }
